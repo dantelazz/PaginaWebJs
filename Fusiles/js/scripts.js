165b231875d0ddf01f9d2622bb0b4b8378628217
dantelazz <69008060+dantelazz@users.noreply.github.com>
2022-08-21 20:56:59 -0300

    Mejorado el sistema de cards

--- Fusiles/js/scripts.js
+++ Fusiles/js/scripts.js
@@ -24,19 +24,20 @@ function cargarJSON() {
       let html = "";
       data.forEach(function (circuito) {
         html += `
-        
-        <div class="card text-center m-5">
-  <div class="card-header text-uppercase">
+        <div class="col">
+        <div class="card text-center bg-light  border-warning m-5">
+  <div class="card-header text-uppercase fw-bold">
   ${circuito.nombre}
   </div>
   <div class="card-body">
   <img src="${circuito.imagen}" class="card-img-top" alt="...">
-    <p class="card-text">${circuito.ubicacion}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text">Ubicacion: ${circuito.ubicacion}</p>
+    <a href="#" class="btn btn-primary">Link</a>
   </div>
   <div class="card-footer text-muted">
     ${circuito.fecha}
   </div>
+</div>
 </div>
         `;
       });
