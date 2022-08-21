c5310df230f3695a9262b1f12944dee1fae633f5
dantelazz <69008060+dantelazz@users.noreply.github.com>
2022-08-21 17:36:54 -0300

    Puse los botones y codie el arrancar

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+//Menu Boton Arrancar
+window.addEventListener('DOMContentLoaded', event => {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle2');
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
