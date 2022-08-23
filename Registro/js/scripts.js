d162357886a8d09e2324bc9d8a926d180490dd14
dantelazz <dantelazzarin@gmail.com>
2022-08-23 10:40:52 -0300

    nada

--- Registro/js/scripts.js
+++ Registro/js/scripts.js
@@ -29,65 +29,18 @@ Swal.fire({
     }
 })
 
-  //Constante para Los formularios
-
-  const autos = {
-  }
-
-  const corredores = {
-
-  }
-
-  // funcion para crear el formulario Si acepta
 
 
-  let formularioSi = function(){
+let formularioSi = function(){
     let html = "";
     html += `
-    <div class="container" id="container1">
-    <div class="col-md-6 col-md-offset-3" id="colorDiv">
-        <div class="col-md-6 col-md-offset-3" id="innerDiv">
-            <div class="page-header">
-                <h3>Log in</h3>
-            </div>
-            <form>
-                <div class="form-group">
-                    <label for="userInput">Username or E-mail</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="passwordConfirm">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    <small><a href="#" id="frgPass">Forgot password?</a></small>
-                </div>
-                <div class="form-group" id="smth">
-                    <label for="securityQuestion">Security Question?</label>
-                    <select name="secQuest" id="dropdown">
-                        <option value="0">Where was your mother born?</option>
-                        <option value="1">What was your first car?</option>
-                        <option value="2">What was the first name of your first pet?</option>
-                        <option value="3">What was your school name?</option>
-                        <option value="4">Where were you born?</option>
-                        <option value="5">What model your first smartphone was?</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="secQuestAnswer">Type your security question here</label>
-                    <input type="text" class="form-control" id="security_question" placeholder="Answer here">
-                </div>
-            </form>
-            <button type="submit" id="submitBtn">Log in</button>
-            <br />
-            <div class="footer">
-                
-            </div>            
-        </div>
-    </div>    
-</div>
+    
         `;
         document.getElementById("Formularios").innerHTML = html;
-  }
+}
 
 
-  formularioSi()
+formularioSi()
   // funcion para crear el formulario No acepta
+
+
