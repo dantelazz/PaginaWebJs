3b078fae5b928fc8d9d4d288089810ba17f70591
dantelazz <69008060+dantelazz@users.noreply.github.com>
2022-09-03 23:44:27 -0300

    Terminado formularios y como obenerlos

--- Registro/js/scripts.js
+++ Registro/js/scripts.js
@@ -27,21 +27,67 @@ window.addEventListener('DOMContentLoaded', event => {
 // })
     
 
+//Data guardada
+let autosGuardados = [];
+let formularioFinal = [];
 
 
-let autos={
-    marca:"",
-    modelo:"",
-    year:"",
-    email:""
-}
+// Esta funcion guarda los datos del formulario en un objeto
+let getdataPrimerSeccion= () => {
 
-let getdata = function(){
+    let autos={
+        marca:"",
+        modelo:"",
+        year:"",
+        email:""
+    }
 
     let marca = document.getElementById("inputmarca").value;
-    console.log(marca)
+    autos.marca = marca;
+    let modelo = document.getElementById("inputmodelo").value;
+    autos.modelo = modelo
+    let year = document.getElementById("inputyear").value;
+    autos.year = year;
+    let email = document.getElementById("inputemail").value;
+    autos.email =email;
 
+    autosGuardados.push(autos)
 }
 
+let getdataSegundaSeccion = () => {
+    let formulario = {
+        pista:"",
+        categoria:"",
+        fecha:"",
+        hora:""
+    }
 
-getdata();
+let pista = document.getElementById("inputPista").value;
+formulario.pista = pista;
+
+let categoria = document.getElementById("inputCategoria").value;
+formulario.categoria = categoria;
+
+let fecha = document.getElementById("inputFecha").value;
+formulario.fecha = fecha;
+
+let hora = document.getElementById("inputHora").value;
+formulario.hora = hora;
+
+formularioFinal.push(formulario)
+}
+
+//Codigo del Sweet Alert
+
+// Swal.fire({
+//     title: 'Tiene su auto registrado?',
+//     text: "La pista es"+formularioFinal.pista,
+//     icon: 'warning',
+//     showCancelButton: true,
+//     confirmButtonColor: '#3085d6',
+//     cancelButtonColor: '#d33',
+//     confirmButtonText: 'Si!',
+//     cancelButtonText: 'No'
+//     ///Para Cambiar el registro
+// })
+    
